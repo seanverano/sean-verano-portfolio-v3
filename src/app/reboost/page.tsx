8b1f48bdd2cdbba8936fa98c90f381f2e0987277
Sean Dustin Verano <verano.seandustin@gmail.com>
2025-02-12 11:45:12 +0800

    Fix bugs in reboost page

--- src/app/reboost/page.tsx
+++ src/app/reboost/page.tsx
@@ -7,7 +7,7 @@ import { FaExternalLinkAlt } from "react-icons/fa";
 
 const faqs = [
   {
-    question: "What&apos;s a brief overview of this project?",
+    question: <>"What&apos;s a brief overview of this project?"</>,
     answer:
       "Reboost is a Chrome extension with two core features, a hydration tracker for logging and tracking water intake and also a reminder for setting custom break alerts.",
   },
