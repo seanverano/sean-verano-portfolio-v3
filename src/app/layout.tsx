8a5d916747244cac0d06c17cb2f330739cbbb986
Sean Dustin Verano <verano.seandustin@gmail.com>
2025-03-01 19:24:32 +0800

    Update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,10 +12,26 @@ const archivo = Archivo({
 export const metadata: Metadata = {
   title: "Sean Dustin Verano | Portfolio",
   description:
-    "Full-stack MERN specialist building premium and results driven products that create value.",
+    "Full-stack MERN specialist building premium and results-driven products that create value.",
   icons: {
-    icon: "/favicon.ico",
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      {
+        url: "/favicon/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/favicon/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      { url: "/favicon/apple-touch-icon.png", rel: "apple-touch-icon" },
+    ],
   },
+  manifest: "/favicon/site.webmanifest",
 };
 
 export default function RootLayout({
